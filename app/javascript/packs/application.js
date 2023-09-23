3b644d0dbbb62bbeedbf6e2ff4eadcf0d7c860aa
louiscao <louiscao17@gmail.com>
2023-09-24 02:17:17 +0700

    Add view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
+require("jquery");
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +16,10 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.jQuery = $;
+window.$ = $;
+
+$("#single-item").slick({
+    dots: true
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.append('jquery', {
+    test: require.resolve('jquery'),
+    use: [{
+        loader: 'expose-loader',
+        options: '$',
+    }, {
+        loader: 'expose-loader',
+        options: 'jQuery',
+    }],
+});
+
 module.exports = environment
